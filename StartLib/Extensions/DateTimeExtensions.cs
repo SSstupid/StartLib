6ada06b006dac9f102635bccc4e743948f329702
SSstupid <rlawoghks3@gmail.com>
2021-09-27 17:46:47 +1000

    Create DateTimeExtensions.cs

---
+++ StartLib/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartLib.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime FirstDateOfMonth(this DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, 1);
+        }
+
+        public static DateTime LastDateOrMonth(this DateTime date)
+        { 
+            return new DateTime(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month));// DaysInMonth => 그달의 날짜수
+        }
+    }
+}
