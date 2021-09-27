fa255c427cf54251f8b88936a06bacfe05c223a6
SSstupid <rlawoghks3@gmail.com>
2021-09-27 17:46:45 +1000

    Create StringExtensions.cs

---
+++ StartLib/Extensions/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartLib.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNumeric(this string s)
+        {
+            long result;
+            return long.TryParse(s, out result);
+        }
+
+        public static bool IsDateTime(this string s)
+        {
+            if (String.IsNullOrEmpty(s))
+                return false;
+            else
+            {
+                DateTime result;
+                return DateTime.TryParse(s, out result);
+            }
+        }
+    }
+}
