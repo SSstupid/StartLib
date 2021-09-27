985ed09882ce91a60fd47d73ba89ee143f553815
SSstupid <rlawoghks3@gmail.com>
2021-09-27 17:45:20 +1000

    Create Application.cs

---
+++ StartLib/Tools/Application.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartLib.Tools
+{
+    public static class Application
+    {
+        public static string Root
+        {
+            get 
+            {
+                return AppDomain.CurrentDomain.BaseDirectory;
+            }
+        }
+    }
+}
